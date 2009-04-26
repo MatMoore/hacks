9d51ab9100fd83bb1d0543bec005f7118e5ed997
bencoder <bencoder@0fdaf05a-3065-11de-9d05-31133e6853b1>
2009-04-26 20:18:53 +0000

    fixed update function

--- pyweek-8/gamelib/world.py
+++ pyweek-8/gamelib/world.py
@@ -29,8 +29,7 @@ class World:
 
     def update(self,dt):
         '''Update the map. Update the objects animations and do collision detection stuff to find out which units are visible to others. Call the interact method of each unit with a list of visible objects.'''
-        def update(self,dt):
-            self.objects.update(dt)
+        self.objects.update(dt)
 
     def draw(self, graphics):
         '''Draw the visible part of the map onto the screen'''
