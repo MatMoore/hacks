e463a330c42c481fbb6cb7b91b9439d94b4a0f15
bencoder <bencoder@0fdaf05a-3065-11de-9d05-31133e6853b1>
2009-05-01 23:02:41 +0000

    fixed location of the health bar

--- pyweek-8/gamelib/world.py
+++ pyweek-8/gamelib/world.py
@@ -107,7 +107,7 @@ class World:
             self.drawHealthBar(graphics,i)
 
     def drawHealthBar(self,graphics,unit):
-        outline = pygame.Rect(unit.rect)
+        outline = pygame.Rect(unit.position[0]-unit.radius,unit.position[1]-unit.radius,unit.radius, unit.radius)
         outline.top -= HEALTHBARGAP
         outline.height = HEALTHBARSIZE[1]
         outline.width = HEALTHBARSIZE[0]
