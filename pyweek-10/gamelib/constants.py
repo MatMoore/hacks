c01a361233ab508c747e0abfed0db39b0de0b919
Mat <MatMoore@users.noreply.github.com>
2010-03-28 16:33:14 +0100

    removed timestep from gameobject

---
+++ pyweek-10/gamelib/constants.py
@@ -0,0 +1,2 @@
+gsQuit = 0
+gsInGame = 10
---
+++ pyweek-10/gamelib/gameobject.py
@@ -0,0 +1,28 @@
+from constants import *
+from numpy import *
+
+class GameObject
+	def __init__():
+		self._position = array([0.0, 0.0, 0.0])
+		self._positionOld = array([0.0, 0.0, 0.0])
+		self._orientation = array([0.0, 0.0, 0.0])	#these might be really bad defaults
+		self._orientationOld = array([0.0, 0.0, 0.0])
+		self.timeSinceUpdate = 0
+
+
+	def update():
+		pass
+
+	def interValues(self, newVal, oldVal)
+		return (newVal + (newVal-oldVal)*self.timeSinceUpdate)
+		
+	def interVector(self, newVec, oldVec)
+		return newVec + (newVec-oldVec)*self.timeSinceUpdate
+
+	@property
+	def position
+		return self.interVector(self._position, self._positionOld)
+		
+	@property
+	def orientation
+		return self.interVector(self._orientation, self._orientationOld)
