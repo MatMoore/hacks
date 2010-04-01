296c2dc0f15bbf863daf432970632a8210691dad
Ben Clark <bencoder@users.noreply.github.com>
2010-04-01 10:19:37 +0100

    camera following unicycle. it's a bit static at the moment. like it's stuck on a rigid metal pole coming out of the back of the unicycle

---
+++ pyweek-10/gamelib/racer.py
@@ -0,0 +1,186 @@
+# If this class doesn't make sense to you don't worry, it doesn't make sense to me either.
+from constants import *
+from numpy import *
+from gameobject import GameObject
+from utils import *
+import pygame
+
+class Racer:
+	def __init__(position,facing,mass=75,height=1.0):
+		self.height = height
+		self.unicycle = Unicycle(position, facing)
+		self.rider = Rider(position + array([0,UNICYCLE_HEIGHT,0]), facing)
+
+	def update(self):
+		#         ahhh!
+		#     o_   /
+		#    _/\\
+		#       \|
+		#      /|~
+		#     _\|_
+		#      /
+		#  ___O_____
+		#
+
+# TODO move this to input code (need to draw a rider first!)
+		# Map mouse movement to rider (not uncycle) tilt
+		#dthetaLR, dthetaFB = map((lambda x: max(pi / 2, x * TILT_ANGLE_PER_PIXEL_PER_SEC * TIMESTEP)), pygame.mouse.get_rel())
+
+		# Update rider orientation
+		#self.rider.lean(dthetaLR, dthetaFB)
+
+		# Ok, our rider is now a perfect sphere sitting on a massless stick. DO NOT QUESTION THIS.
+		#     o   <-- rider's mass
+		#      \
+		#       \  <-- uncicyle/human hybrid
+		#        \
+		#         O <-- wheel
+
+		# Calculate vector from wheel to rider
+		stickVector = self.unicycle.orientation * UNICYCLE_HEIGHT + 0.5 * self.rider.height * self.rider.orientation
+
+		# Project that into the forward-y plane
+		stickVector = self.unicycle.rightYProjection(stickVector)
+
+		# Get current angle of the stick from vetical
+		#     o
+		#     :\
+		#     :t\
+		#     :--\
+		#     :   O
+		theta = angleBetween(stickVector, y)
+
+		# Now we will attempt to calculate the new angle of the system. Assuming that the unicycle will rotate due to a) acceleration of the wheel and b) torque due to the riders COM
+
+		# Equation of motion (THIS IS PROBABLY WRONG):
+		# mL^2 alpha = m L sin theta - M a L cos theta
+		# L alpha = g sin theta - M/m a cos theta
+		# L alpha = g sin theta - CONST a cost theta
+		# where alpha is angular acceleration of stickvector, theta is angle of stickvector relative to the vertical, L is the length of the stick vector, g is graviation acceleration and a is wheel acceleration
+		alpha = g * sin(theta) - UNICYCLE_MASS / self.rider.mass *self.unicycle.acceleration * cos(theta)
+
+		# integrate to get new angle
+		newTheta, newAngularVel = integrate(theta,alpha,self.unicycle.angularVel)
+		dtheta = newTheta - theta
+
+		# Ok lets work out what the actual unicycle angle would be to make this sphereonstick angle. Btw I am assuming rider is not wibbly wobbly indepent of the uni.
+		
+		# update unicycle orientation
+		self.unicycle.thetaFB += dtheta
+
+		# update unicycle position using wheel accn
+		unicycle.move()
+
+		# update rider position by sticking him back on the top of the unicycle
+		self.rider.position = self.unicycle.position + unicycle.orientation * UNICYCLE_HEIGHT
+
+
+class WibblyWobbly(GameObject):
+	'''Directions/angles:
+	Forward,backward,left,right are all unit vectors in the X-Z plane, describing which way the unicycle wheel is moving
+	Orientation is the "up" unit vector, and describes how the wobbly is tilted
+thetaFB is the angle from vertical the left-up plane is rotated
+thetaLR is the angle from vertical the forward-up plane is rotated
+	'''
+	def __init__(self, position, facing):
+
+		orientation = array([0, 1, 0]) # Start upright
+
+		# Work out the direction we're moving in
+		# The unicycle starts vertical, so the "facing" angle is the angle from the x axis.
+		self.forward = dot(array([1,0,0]), rotationMatrix(array([0,1,0]), facing))
+		GameObject.__init__(self, position,orientation,facing)
+
+	@property
+	def left(self):
+		return cross(y, self.forward)
+
+	@property
+	def right(self):
+		return -self.left
+
+	@property
+	def backward(self):
+		return -self.forward
+
+	def turnRight(self, angle):
+		self.forward = dot(self.forward, rotationMatrix(y, angle))
+		self.orientation = dot(self.orientation, rotationMatrix(y,angle))
+
+	def turnLeft(self, angle):
+		self.turnRight(-angle)
+
+	def forwardYProjection(self, vector):
+		'''Return the component of the vector in the forward-Y plane'''
+		# A || B = B x (AxB / |B|) / |B|
+		# where A is vector B is normal to the plane
+		normal = cross(self.forward, y)
+		return cross(normal, (cross(vector,normal)/linalg.norm(normal))) / linalg.norm(normal)
+
+	def rightYProjection(self, vector):
+		'''Return the component of the vector orientation in the right-Y plane'''
+		normal = cross(self.right, y)
+		return cross(normal, (cross(vector,normal)/linalg.norm(normal))) / linalg.norm(normal)
+
+	@property
+	def thetaLR(self):
+		'''The angle between the forward-up unicycle plane and the y axis.'''
+		return angleBetween(self.forwardYProjection(self.orientation), y)
+
+	@thetaLR.setter
+	def thetaLR(self, value):
+		'''Rotate around the forward vector'''
+
+		# Go back to vertical
+		self.orientation = dot(self.orientation, rotationMatrix(self.forward, -self.thetaLR))
+
+		# Rotate to new angle
+		self.orientation = dot(self.orientation, rotationMatrix(self.forward, value))
+
+	@property
+	def thetaFB(self):
+		'''The angle between the left-up unicycle plane and the y axis'''
+		# This is 90 deg - (angle between the normal and the y axis)
+		return angleBetween(self.rightYProjection(self.orientation), y)
+
+	@thetaFB.setter
+	def thetaFB(self, value):
+		'''Rotate around the left vector'''
+
+		# Go back to vertical
+		self.orientation = dot(self.orientation, rotationMatrix(self.left, -self.thetaFB))
+
+		# Rotate to new angle
+		self.orientation = dot(self.orientation, rotationMatrix(self.left, value))
+
+
+class Rider(GameObject):
+	def __init__(self, position, facing):
+		orientation = array([0, 1, 0]) # Start upright
+		self.forward = dot(array([1,0,0]), rotationMatrix(array([0,1,0]), facing))
+		GameObject.__init__(self, position,orientation,facing)
+
+	def lean(dThetaLR, dThetaFB):
+		self.thetaLR += dThetaLR
+		self.thetaFB += dThetaFB
+
+class Unicycle(WibblyWobbly):
+	def __init__(self,position, facing=0):
+		self.speed = 0 # wheel speed
+		self.acceleration = 0 # wheel acceleration
+		self.angularVel = 0 # angular velocity of frame
+		WibblyWobbly.__init__(self,position,facing)
+
+	def move(self):
+		'''Apply the wheel acceleration'''
+		newPos, newVel = integrate(self.position, self.speed * self.forward, self.acceleration)
+		newSpeed = linalg.norm(newVel)
+		self.position = newPos
+		self.speed = newSpeed
+
+if __name__ == "__main__":
+	unicycle = Unicycle(array([0,0,0]), 0)
+	print unicycle.thetaFB
+	print math.pi * 0.25
+	unicycle.thetaFB = math.pi*0.25
+	print unicycle.thetaFB
---
+++ pyweek-10/gamelib/track.py
@@ -0,0 +1,151 @@
+import constants
+import pickle
+import math
+from numpy import *
+
+class Track:
+	def __init__(self, loadTrack = None):
+		if loadTrack:
+			fileHandle = open(loadTrack, 'r')
+			self.track = pickle.load(fileHandle)
+			self.makeOutsideTrack()
+
+	def lineLineIntersection(self, p1, p2, p3, p4, segment = False):
+		#from http://local.wasp.uwa.edu.au/~pbourke/geometry/lineline2d/
+		denom = (p4[1] - p3[1]) * (p2[0] - p1[0]) - (p4[0] - p3[0]) * (p2[1] - p1[1])
+		if denom == 0:	
+			return array([False])															#if the denom is 0 it means the lines are parallel so return this and we can check for the case
+		numer1 = (p4[0] - p3[0]) * (p1[1] - p3[1]) - (p4[1] - p3[1]) * (p1[0] - p3[0])
+		m1 = numer1/denom
+		x = p1[0] + m1*(p2[0]-p1[0])
+		y = p1[1] + m1*(p2[1]-p1[1])
+		if segment:
+			#for segment calculation both m1 and m2 must be between 0 and 1
+			if m1 >= 0 and m1 <= 1:
+				numer2 = (p2[0] - p1[0]) * (p1[1] - p3[1]) - (p2[1] - p1[1]) * (p1[0] - p3[0])		#we onyl need to do this part if we are doing segment-segment checking
+				m2 = numer2/denom
+				if not (m2 >= 0 and m2 <= 1):
+					return array([False])
+			else:
+				return array([False])
+
+		return array([x,y])
+
+	def makeOutsideTrack(self):
+		self.quadPoints = []			#this is an openGl specific thing for how to draw the but i think it's better to store here than in the camera and def. better than generating it each frame
+		self.outsideTrack = []
+		for i in range(len(self.track)):
+			if i == len(self.track)-1:	#if i is at the end then:
+				p3 = self.track[0]		#get the first point
+			else:
+				p3 = self.track[i+1]
+			p1 = self.track[i-1]	#this works because [-1] index will give last result
+			p2 = self.track[i]
+			#we now have p1, p2 and p3 which make 2 lines. we are calculating the outside point at p2
+			#get the vectors for both lines
+			dp12 = p2-p1
+			dp23 = p3-p2
+			
+			# get the normal unit vector for both lines
+			n1 = array([dp12[1]*-1, dp12[0]])
+			n1 = n1 / linalg.norm(n1)
+			n2 = array([dp23[1]*-1, dp23[0]])
+			n2 = n2 / linalg.norm(n2)
+			
+			#now use the normal vector to shift both lines outwards
+			iP1 = p1 + n1*constants.roadWidth
+			iP2 = p2 + n1*constants.roadWidth
+			iP3 = p2 + n2*constants.roadWidth
+			iP4 = p3 + n2*constants.roadWidth
+			
+			#the above 4 points refer to the 2 outer lines that make the angle, now we need to find the intersection, to find the inside point
+			#insidePoint = geometry.getIntersectPoint(iP1, iP2, iP3, iP4)
+			point = self.lineLineIntersection(iP1, iP2, iP3, iP4)
+			
+			if point.all() == False:
+				point = iP2			#it returns false if the lines are parallel (which could potentially happen) so here we set it to the point in the middle(either iP2 or iP3, should be the same)
+				
+			self.outsideTrack.append(point)
+			self.quadPoints.append(self.track[i])
+			self.quadPoints.append(point)
+
+		#these last two just join it back up to the beginning
+		self.quadPoints.append(self.track[0])
+		self.quadPoints.append(self.outsideTrack[0])
+
+
+	def generateTrack(self, complexity = 10, size = 50):		#complexity defines the number of points for the track, size = the average distance from the center of the map.
+		points = []
+		inside = []
+		for i in range(complexity):
+			angle = 2*math.pi * i/complexity		#get the angle around the circle
+			distance = random.normal(size, size/5)	#get a random distance based around a normal distribution
+			point = array([distance*math.sin(angle), distance*math.cos(angle)])
+			points.append(point)
+			if (i==0):
+				self.startingPoint = (point[0]+constants.roadWidth/2, point[1]+constants.roadWidth/2)
+		self.track = points
+		self.makeOutsideTrack()
+
+	def saveTrack(self, filename):
+		fileHandle = open(filename, 'w')
+		pickle.dump(self.track, fileHandle)
+
+	def isInsidePoly(self, position, polygon):
+		#make two points at far left and right offset in the x direction, this gives two points on a horizontal line
+		#these are clearly magic numbers and may need to be increased to stretch the segment out further
+		#i'm also modifying the Y position up on one side and down on the other, this puts a slight tilt to it and avoids problems where we're directly on a horizontal node
+		#there are better solutions here: http://alienryderflex.com/polygon/ but this is a quick hack that probably covers us
+		
+		p1 = (position[0]-10000, position[1]-500)
+		p2 = (position[0]+10000, position[1]+500)
+		
+		intersectionsLeft = 0
+		#Its a fairly simple algorithm, you take the intersections of a single line raycasted from the point and then determine if theres an odd number on each side, or an even number
+		#if it's odd then you're inside the polygon, if it's even(or 0) then you're outside. We're only checking the left hand side, since you only need to check one side
+		for i in range(len(polygon)):
+			if i == len(polygon)-1:	#handle the last element in the list
+				i2 = 0
+			else:
+				i2 = i+1
+			intersection = self.lineLineIntersection(p1, p2, polygon[i], polygon[i2], True)	#the true on the end makes this segment-segment intersection
+			if intersection.all() != False:
+				if intersection[0] < position[0]:
+					intersectionsLeft += 1
+		if (intersectionsLeft % 2) == 1:	#if not divisible by 2(odd)
+			return True						#then we're inside the poly
+		else:
+			return False
+
+	def getTerrain(self, position):				#position is just a 2d coordinate x, y, to determine if a point is on the track or off the track(this can be extended to different types of track terrain)
+		#it needs to be inside the outer track and outside the inside track
+		if (self.isInsidePoly(position, self.outsideTrack) and not self.isInsidePoly(position, self.track)):
+			#it is on the track
+			return True
+		else:
+			return False	#TODO: make a bunch of constants for terrain types
+
+
+if __name__ == "__main__":
+	track = Track()
+	track.generateTrack(20,100)
+	import pygame
+	pygame.init()
+	window = pygame.display.set_mode((640, 640)) 
+	for i in range(len(track.track)):
+		if i == (len(track.track)-1):
+			z = 0
+		else:
+			z = i+1
+		pygame.draw.line(window, (255,255,255), (track.track[i][0]*2+300, track.track[i][1]*2+300), (track.track[z][0]*2+300, track.track[z][1]*2+300))
+		pygame.draw.line(window, (255,0,0), (track.outsideTrack[i][0]*2+300, track.outsideTrack[i][1]*2+300), (track.outsideTrack[z][0]*2+300, track.outsideTrack[z][1]*2+300))		
+	pygame.display.flip()
+	while True: 
+		pygame.time.wait(50)
+		for event in pygame.event.get(): 
+			if event.type == pygame.QUIT: 
+				exit()
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				point = event.pos
+				point = ((point[0]-300)/2, (point[1]-300)/2)	#convert it back into terrain coordinates
+				print track.getTerrain(point)
