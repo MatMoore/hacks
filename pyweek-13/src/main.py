c0a8f3cf5c39546ef7b15f1b8a4c2abc3933438b
Mat Moore <matmoore@users.noreply.github.com>
2011-09-11 12:21:50 +0100

    Player images

--- pyweek-13/src/main.py
+++ pyweek-13/src/main.py
@@ -1,9 +1,20 @@
 import config
+import sys
 from logging import info,debug,error
 config.setupLogging()
 
+try:
+	import pygame
+except ImportError:
+	print 'This game requires pygame to run'
+	sys.exit(1)
+
+def pygame_init():
+	pass
+
 def main():
 	info('Starting '+config.get('Project', 'name')+'...')
+	pygame_init()
 	game = Controller()
 	while game.tick():
 		pass
