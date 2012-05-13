94a18f0537fb26f95959bab7cc840340fd1d836e
Mat Moore <matmoore@users.noreply.github.com>
2012-05-13 11:35:22 +0100

    distribute stuff

---
+++ pyweek-14/setup.py
@@ -0,0 +1,17 @@
+setup(
+	name='Grey Goo',
+	version='1.1.1',
+	packages=find_packages(),
+	install_requires=[
+		'pygame>=1.9.1',
+		'numpy>=1.3',
+	],
+	package_data={
+		'gamename': ['data/*'],
+	},
+	entry_points={
+		'console_scripts': [
+			'GreyGoo = src.main:main',
+		]
+	}
+)
