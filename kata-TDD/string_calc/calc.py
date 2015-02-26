9a615f3c058d7499125edfd81147088beffaf55b
Mat Moore <matmoore@users.noreply.github.com>
2015-02-26 12:45:59 +0000

    Single number

--- kata-TDD/string_calc/calc.py
+++ kata-TDD/string_calc/calc.py
@@ -1,3 +1,6 @@
 def add(numbers):
     if not numbers:
         return 0
+
+    numberlist = numbers.split(',')
+    return sum(float(i) for i in numberlist)
--- kata-TDD/string_calc/test_calc.py
+++ kata-TDD/string_calc/test_calc.py
@@ -6,3 +6,19 @@ def test_add_empty_is_zero():
     For an empty string, add returns 0
     """
     assert add('') == 0
+
+
+def test_none_is_zero():
+    """
+    None is treated the same as empty string
+    """
+    assert add(None) == 0
+
+
+def test_one_int():
+    """
+    A single number is returned as a float
+    """
+    result = add('1')
+    assert result == 1
+    assert isinstance(result, float)
