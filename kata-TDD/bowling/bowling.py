3b7a023d6a45a13e3501ed38a9a76a56e1f15fea
Mat Moore <matmoore@users.noreply.github.com>
2015-02-27 18:27:04 +0000

    Third test - spares

--- kata-TDD/bowling/bowling.py
+++ kata-TDD/bowling/bowling.py
@@ -1,9 +1,30 @@
 class Game(object):
     def __init__(self):
-        self._score = 0
+        self.rolls = []
 
     def roll(self, number):
-        self._score += number
+        self.rolls.append(number)
+
+    @property
+    def complete_frames(self):
+        return zip(self.rolls[::2], self.rolls[1::2])
 
     def score(self):
-        return self._score
+        total = 0
+        frames = self.complete_frames
+        for frame_number in xrange(0, len(frames)):
+            frame = frames[frame_number]
+            total += sum(frame)
+
+            try:
+                next_frame = frames[frame_number + 1]
+            except IndexError:
+                pass
+            else:
+                if self.is_spare(frame):
+                    total += next_frame[0]
+
+        return total
+
+    def is_spare(self, frame):
+        return sum(frame) == 10
--- kata-TDD/bowling/test_bowling.py
+++ kata-TDD/bowling/test_bowling.py
@@ -10,6 +10,10 @@ class GameHelper(Game):
         for i in xrange(n):
             self.roll(pins)
 
+    def roll_spare(self, first=5):
+        self.roll(first)
+        self.roll(10 - first)
+
 
 @pytest.fixture
 def game():
@@ -24,3 +28,16 @@ def test_gutter_game(game):
 def test_all_ones(game):
     game.roll_many(20, pins=1)
     assert game.score() == 20
+
+
+def test_one_spare(game):
+    game.roll_spare()
+    game.roll(3)
+    game.roll_many(17, pins=0)
+    assert game.score() == 16
+
+
+#def test_strike(game):
+#    game.roll(10)
+#    game.roll(3)
+#    game.roll(4)
