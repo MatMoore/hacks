ae7bcd5ac8fe2f3f409d15d502be2de10fee1a71
Mat Moore <matmoore@users.noreply.github.com>
2015-02-27 17:51:12 +0000

    Factor out repeated test code
    I'm using a wrapper object for the fixture, rather than a test suite method,
    because pytest prefers dependency injection at the individual test level rather
    than xUnit style setup/teardown and instance attributes.

--- kata-TDD/bowling/test_bowling.py
+++ kata-TDD/bowling/test_bowling.py
@@ -2,18 +2,25 @@ import pytest
 from bowling import Game
 
 
+class GameHelper(Game):
+    """
+    Add some helper functions to our game for testing
+    """
+    def roll_many(self, n, pins):
+        for i in xrange(n):
+            self.roll(pins)
+
+
 @pytest.fixture
 def game():
-    return Game()
+    return GameHelper()
 
 
 def test_gutter_game(game):
-    for i in xrange(20):
-        game.roll(0)
+    game.roll_many(20, pins=0)
     assert game.score() == 0
 
 
 def test_all_ones(game):
-    for i in xrange(20):
-        game.roll(1)
+    game.roll_many(20, pins=1)
     assert game.score() == 20
