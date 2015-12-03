1d7c8f85732d074ea9b8e885ac204dcb7023f07e
Mat Moore <matmoore@users.noreply.github.com>
2015-12-03 23:39:44 +0000

    day3

--- advent-of-code-2015/src/xmas/core.clj
+++ advent-of-code-2015/src/xmas/core.clj
@@ -1,5 +1,6 @@
 (ns xmas.core
   (:require [clojure.string :refer [split]])
+  (:require [clojure.set :refer [union]])
   (:gen-class))
 
 (defn parse-int
@@ -17,6 +18,11 @@
 
 (def day2-dimensions
   (map #(map parse-int (split % #"x")) day2-data))
+
+(def day3-data
+  (filter
+    #(contains? (set ">v<^") %)
+    (slurp "day3.txt")))
 
 (defn take-elevator
   "Go to the floor indicated by the symbol"
@@ -96,7 +102,49 @@
 (def total-ribbon-length
   (reduce + (map present-ribbon-length day2-dimensions)))
 
+(def sleigh-directions
+  {\> [1 0]
+   \v [0 1]
+   \< [-1 0]
+   \^ [0 -1]})
+
+(defn translate-sleigh
+  "Move Santas sleigh according one space in the given direction"
+  [[x y] direction]
+  (let [diff (get sleigh-directions direction)]
+    [(+ x (first diff)) (+ y (second diff))]))
+
+(defn register-sleigh-movement
+  "Update a count of visited houses, given the current position and a direction"
+  [[history current-pos] direction]
+  (let [new-pos (translate-sleigh current-pos direction)]
+    [(assoc-in history [new-pos] (+ 1 (get history new-pos 0)))
+     new-pos]))
+
+(defn count-sleigh-visits
+  "Count every house Santa visits, given a sequence of directions"
+  [directions]
+  (reduce register-sleigh-movement [{[0 0] 1} [0 0]] directions))
+
+(def visited-houses
+  "Get the set of visited houses for a sequence of directions"
+  (comp set keys first count-sleigh-visits))
+
+(def at-least-one-present
+  "Number of houses with at least one present pre-robo-santa"
+  (count (first (count-sleigh-visits day3-data))))
+
+(def human-santa-directions (take-nth 2 day3-data))
+(def robo-santa-directions (take-nth 2 (rest day3-data)))
+
+(def assisted-least-one-present
+  "Number of houses with at least one present post-robo-santa"
+  (count
+    (union
+      (visited-houses human-santa-directions)
+      (visited-houses robo-santa-directions))))
+
 (defn -main
   "Solve the last task"
   [& args]
-  (println (pr-str total-ribbon-length)))
+  (println (pr-str assisted-least-one-present)))
