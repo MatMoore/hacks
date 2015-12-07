604b556ef9cba6eaffe8069241eb089a709c40b9
Mat Moore <matmoore@users.noreply.github.com>
2015-12-07 15:26:51 +0000

    day7

---
+++ advent-of-code-2015/test/xmas/core_test.clj
@@ -0,0 +1,18 @@
+(ns xmas.core-test
+  (:require [clojure.test :refer :all]
+            [xmas.core :refer :all]))
+
+(def test-data
+  (with-open [rdr (clojure.java.io/reader "day7test.txt")]
+    (into {} (map parse-expression (line-seq rdr)))))
+
+(deftest example-data
+  (is (= (evaluate-wires test-data "d") 72))
+  (is (= (evaluate-wires test-data "e") 507))
+  (is (= (evaluate-wires test-data "f") 492))
+  (is (= (evaluate-wires test-data "g") 114))
+  (is (= (evaluate-wires test-data "h") 65412))
+  (is (= (evaluate-wires test-data "i") 65079))
+  (is (= (evaluate-wires test-data "x") 123))
+  (is (= (evaluate-wires test-data "y") 456))
+)
