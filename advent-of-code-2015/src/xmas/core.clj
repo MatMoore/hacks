c451e3ca40d512e6b11d05e3a4a9d1d4c4c57edd
Mat Moore <matmoore@users.noreply.github.com>
2015-12-07 01:50:42 +0000

    day6, with extra python implementation

--- advent-of-code-2015/src/xmas/core.clj
+++ advent-of-code-2015/src/xmas/core.clj
@@ -8,14 +8,18 @@
   [s]
   (Integer/parseInt s))
 
+(defn read-lines
+  [filename]
+  (with-open [rdr (clojure.java.io/reader filename)]
+    (into [] (line-seq rdr))))
+
 (def task1-data (slurp "task1.txt"))
 
 (def sanitised-data
   (filter #(contains? (set "()") %) task1-data))
 
 (def day2-data
-  (with-open [rdr (clojure.java.io/reader "day2.txt")]
-    (into '() (line-seq rdr))))
+  (read-lines "day2.txt"))
 
 (def day2-dimensions
   (map #(map parse-int (split % #"x")) day2-data))
@@ -26,8 +30,10 @@
     (slurp "day3.txt")))
 
 (def day5-data
-  (with-open [rdr (clojure.java.io/reader "day5.txt")]
-    (into '() (line-seq rdr))))
+  (read-lines "day5.txt"))
+
+(def day6-data
+  (read-lines "day6.txt"))
 
 (defn take-elevator
   "Go to the floor indicated by the symbol"
@@ -186,7 +192,76 @@
 (def number-nice
   (count nice-list))
 
+(defn toggle-light
+  [lights coords]
+  (assoc-in
+    lights [coords]
+    (not (get lights coords false))))
+
+(defn set-light
+  [state lights coords]
+  (assoc-in lights [coords] state))
+
+(defn set-light2
+  [diff lights coords]
+  (assoc-in lights [coords] (max 0 (+ diff (get lights coords 0)))))
+
+(def turn-on-light2
+  (partial set-light2 1))
+
+(def turn-off-light2
+  (partial set-light2 -1))
+
+(def toggle-light2
+  (partial set-light2 2))
+
+(def turn-on-light
+  (partial set-light true))
+
+(def turn-off-light
+  (partial set-light false))
+
+(defn rect
+  [[left top right bottom]]
+  (for
+    [x (range
+         left (+ 1 right))
+     y (range
+         top (+ 1 bottom))]
+    [x y]))
+
+(defn run-light-instruction
+  "Apply an instruction to the lights and return their new state"
+  [lights [command corners]]
+  (reduce command lights (rect corners)))
+
+(def lights-regex
+  #"(turn on|turn off|toggle) (\d+),(\d+) through (\d+),(\d+)")
+
+(defn parse-light-instruction
+  [line]
+  (let [[match command & corners] (re-find lights-regex line)]
+    (case command
+      "turn on" [turn-on-light2 (map parse-int corners)]
+      "turn off" [turn-off-light2 (map parse-int corners)]
+      "toggle" [toggle-light2 (map parse-int corners)])))
+
+(defn run-light-sequence
+  "Follow Santa's instructions for setting up the lights"
+  [instructions]
+  (reduce
+    run-light-instruction
+    {}
+    (map parse-light-instruction instructions)))
+
+(defn total-brightness
+  "Count the total brightness after following Santa's instructions"
+  [instructions]
+  (reduce +
+    (vals
+      (run-light-sequence instructions))))
+
 (defn -main
   "Solve the last task"
   [& args]
-  (println (pr-str number-nice)))
+  (println (pr-str (total-brightness day6-data))))
