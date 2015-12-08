7757ff513ebd182605866cfdf8831eaa06b3d259
Mat Moore <matmoore@users.noreply.github.com>
2015-12-08 21:01:54 +0000

    day8 part 2

--- advent-of-code-2015/src/xmas/core.clj
+++ advent-of-code-2015/src/xmas/core.clj
@@ -344,15 +344,34 @@
 
 (def day8-data (read-lines "day8.txt"))
 
-(def replace-hex (fn [string] (replace string #"\\x[0-9a-fA-F]{2}" "X")))
+(def replace-hex
+  "Because the reader doesn't parse hex literals, just replace them with another letter"
+  (fn [string] (replace string #"\\x[0-9a-fA-F]{2}" "X")))
 
-(def day8-parsed (map read-string (map replace-hex day8-data)))
+(defn parse-list
+  "Use the reader to decode Santa's list. They're almost the same as clojure string literals."
+  [data]
+  (map read-string (map replace-hex data)))
 
-(def diffs (map - (map count day8-data) (map count day8-parsed)))
+(defn name-diffs
+  "Diff the length of the names before and after decoding"
+  [data-before data-after] (map - (map count data-before) (map count data-after)))
 
-(defn day8-result [diffs] (reduce + diffs))
+(defn total-name-diffs
+  [data-before data-after] (reduce + (name-diffs data-before data-after)))
+
+(defn double-encode-name
+  "Escape all double quotes and backslashes with another backslash"
+  [string]
+  (str
+    "\""
+    (replace string #"[\\\"]" #(str "\\" %))
+    "\""))
+
+(defn double-encode-list [data]
+  (map double-encode-name data))
 
 (defn -main
   "Solve the last task"
   [& args]
-  (println (pr-str (day8-result diffs))))
+  (println (pr-str (total-name-diffs (double-encode-list day8-data) day8-data))))
