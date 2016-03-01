eea564f7f0d368804e262b33f4f864da1691854b
Mat Moore <matmoore@users.noreply.github.com>
2016-03-01 23:26:47 +0000

    http://cryptopals.com/sets/1/challenges/1/

---
+++ crypto-challenge/encode.rb
@@ -0,0 +1,19 @@
+module EncodeHacks
+    refine String do
+        def decode_hex
+            [self].pack('H*')
+        end
+
+        def encode_base64
+            [self].pack('m0')
+        end
+    end
+end
+
+if __FILE__ == $0
+    using EncodeHacks
+    hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    encoded = hex.decode_hex.encode_base64
+    puts encoded
+    puts encoded == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+end
