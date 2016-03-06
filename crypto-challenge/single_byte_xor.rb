f6cecc774d2beb6ea57d5017bf00475416353490
Mat Moore <matmoore@users.noreply.github.com>
2016-03-06 10:18:03 +0000

    http://cryptopals.com/sets/1/challenges/4/

--- crypto-challenge/single_byte_xor.rb
+++ crypto-challenge/single_byte_xor.rb
@@ -10,7 +10,7 @@ module SingleByteXor
         best = nil
         best_score = 0
 
-        ('A'..'z').each do |key|
+        ("\x00".."\x7f").each do |key|
             full_key = key * cyphertext.length
             plaintext = cyphertext.xor(full_key)
             score = scorer.score_text(plaintext)
@@ -20,7 +20,7 @@ module SingleByteXor
             end
         end
 
-        best
+        [best, best_score]
     end
 
     if __FILE__ == $0
@@ -28,5 +28,9 @@ module SingleByteXor
 
         cyphertext = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736".decode_hex
         puts best_guess(cyphertext)
+
+        scorer = Scoring::dictionary_scorer
+        possible_ct = IO.readlines('4.txt').map(&:strip).map {|x| x.decode_hex}
+        puts possible_ct.map {|ct| best_guess(ct, scorer:scorer)}.max_by(&:last)
     end
 end
