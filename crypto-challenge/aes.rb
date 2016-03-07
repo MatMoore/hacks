ff0cfb27cdcaa9e6cf0b0b18d48e6cc57b902e09
Mat Moore <matmoore@users.noreply.github.com>
2016-03-07 22:02:07 +0000

    http://cryptopals.com/sets/1/challenges/8/

--- crypto-challenge/aes.rb
+++ crypto-challenge/aes.rb
@@ -1,5 +1,19 @@
 require_relative 'encode'
 require "OpenSSL"
+require "set"
+
+module AESDetector
+    def self.detect(line)
+        seen = Set.new
+        line.chars.each_slice(16).each do |block|
+            return true if seen.include? block
+
+            seen.add(block)
+        end
+
+        false
+    end
+end
 
 if __FILE__ == $0
     using EncodeHelpers
@@ -8,4 +22,10 @@ if __FILE__ == $0
     ciphertext = IO.read("7.txt").gsub("\n", "").decode_base64
     decipher.key = "YELLOW SUBMARINE"
     puts decipher.update(ciphertext) + decipher.final
+
+    puts "---"
+
+    File.foreach("8.txt") do |line|
+        puts "Detected repeated block: #{line}" if AESDetector.detect(line.strip.decode_hex)
+    end
 end
