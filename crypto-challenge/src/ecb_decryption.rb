f97e3c82c4bdaa7e951b345e56c74e84e6ebceff
Mat Moore <matmoore@users.noreply.github.com>
2016-04-24 16:33:38 +0100

    Extend ECB decrypter to handle arbitrary prefixes
    This is a bit of a mess at the moment, maybe we can factor
    out some of the complexity in decrypt_block

--- crypto-challenge/src/ecb_decryption.rb
+++ crypto-challenge/src/ecb_decryption.rb
@@ -21,7 +21,7 @@ module EcbDecryption
     class EcbPrefixOracle
         def initialize(blocksize: 16)
             key = SecureRandom.random_bytes(blocksize)
-            @random_prefix = SecureRandom.random_bytes(SecureRandom.random_number(2 * blocksize))
+            @random_prefix = SecureRandom.random_bytes(1 + SecureRandom.random_number(2 * blocksize))
             @cipher = Block::AesEcbCipher.new(key)
             @unknown_string = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK".decode_base64
         end
@@ -57,33 +57,42 @@ module EcbDecryption
             highest_jump + 1
         end
 
-        # Find how many random blocks there are in front of the attacker
-        # controlled string in the plaintext. We can find this in the
-        # ciphertext by sending two blocks that are the same
-        def count_prefix_blocks
-            blocksize = determine_blocksize
-            attacker_text = "A" * blocksize * 2
-            ciphertext = @oracle.encrypt(attacker_text)
+        # Find how many random bytes there are in front of the attacker
+        # controlled string in the plaintext. We can see this in the ciphertext
+        # by repeating a character and gradually increasing the size of the
+        # string until we see two repeated blocks. This tells us how many
+        # random prefix blocks there are, and how many bytes to prefix
+        # with to ensure that our attacker controller text aligns with
+        # block boundaries.
+        def measure_prefix(blocksize)
+            (0...blocksize).each do |byte_offset|
+                attacker_text = "A" * (blocksize * 2 + byte_offset)
+                ciphertext = @oracle.encrypt(attacker_text)
 
-            ct_blocks = ciphertext.chars.each_slice(blocksize).to_a
-            ct_blocks.zip(ct_blocks[1..-1]).each_with_index do |(current_block, next_block), i|
-                if current_block == next_block
-                    return i
+                ct_blocks = ciphertext.chars.each_slice(blocksize).to_a
+                ct_blocks.zip(ct_blocks[1..-1]).each_with_index do |(current_block, next_block), block_number|
+                    if current_block == next_block
+                        return [block_number, byte_offset]
+                    end
                 end
             end
 
-            nil
+            throw "Failed to create duplication in ciphertext blocks"
         end
 
         def decrypt
             output = ""
             blocksize = determine_blocksize
             block = nil
+            prefix_blocks, alignment_prefix = measure_prefix(blocksize)
+
             0.step do |block_number|
                 block, last = decrypt_block(
                     block_number: block_number,
                     blocksize: blocksize,
-                    preceding_block: block
+                    preceding_block: block,
+                    alignment_prefix: "A" * alignment_prefix,
+                    prefix_blocks: prefix_blocks
                 )
                 output << block
                 break if last
@@ -92,21 +101,33 @@ module EcbDecryption
             output
         end
 
-        def decrypt_block(block_number:, blocksize:, preceding_block: nil)
+        # block_number is the block number to decrypt minus the number of prefix blocks
+        # prefix_blocks is the number of blocks from our random prefix + alignment prefix
+        # alignment_prefix is a prefix we insert into the attacker text, to make the random
+        # prefix align with block boundaries.
+        # preceding_block is the last block decrypted
+        def decrypt_block(block_number:, blocksize:, alignment_prefix: "", prefix_blocks: 0, preceding_block: nil)
             decrypted = ""
             preceding_block ||= "A" * blocksize
-            block_start = block_number * blocksize
+
+            # Work out block boundaries for the block we're decrypting
+            block_start = (prefix_blocks + block_number) * blocksize
             next_block_start = block_start + blocksize
+
+            # Work out block boundaries for a test block we create
+            # This block is right after the random and alignment prefixes
+            test_block_start = prefix_blocks * blocksize
+            next_test_block_start = test_block_start + blocksize
+
             is_last_block = false
 
             (1..blocksize).each do |byte_number|
                 # Shift the next unknown byte into the last byte of the
                 # preceding block, which is known - it's either the last block
                 # decrypted or the prefix itself.
-                n_bytes_short = preceding_block[byte_number, blocksize]
-                byte_short_prefix = n_bytes_short
+                preceding_part = preceding_block[byte_number, blocksize]
 
-                oracle_result = @oracle.encrypt(byte_short_prefix)
+                oracle_result = @oracle.encrypt(alignment_prefix + preceding_part)
 
                 # Extract the block of ciphertext we're examining.
                 # This corresponds to the plaintext we manipulated above to
@@ -116,13 +137,13 @@ module EcbDecryption
 
                 # Use the oracle to test all possible plaintexts for the block
                 # we're interested in, by varying the last byte.
-                # Since this is ECB, we can place our test string at the start
-                # and just ignore the ciphertext blocks after the first one.
                 ("\x00".."\x7f").each do |char|
-                    test_prefix = byte_short_prefix + decrypted + char
-                    test_result = @oracle.encrypt(test_prefix)
+                    test_result = @oracle.encrypt(
+                        alignment_prefix + preceding_part + decrypted + char
+                    )
+                    test_result_block = test_result[test_block_start...next_test_block_start]
 
-                    if test_result[0...blocksize] == oracle_block
+                    if test_result_block == oracle_block
                         decrypted << char
                         break
                     end
@@ -138,12 +159,23 @@ module EcbDecryption
     end
 
     if __FILE__ == $0
+        puts "Byte-at-a-time ECB decryption (easy)"
         oracle = EcbOracle.new
         decryptor = EcbDecryptor.new(oracle)
         puts "Block size = #{decryptor.determine_blocksize}"
-        puts "Attacker controlled text starts at block #{decryptor.count_prefix_blocks}"
+        puts "Random prefix: #{decryptor.measure_prefix(decryptor.determine_blocksize)}"
         puts "Using ECB = #{ECBDetector.detect(oracle.encrypt("a" * 200))}"
         puts decryptor.decrypt
+
+        puts
+        puts "Byte-at-a-time ECB decryption (harder)"
+        oracle = EcbPrefixOracle.new
+        decryptor = EcbDecryptor.new(oracle)
+        puts "Block size = #{decryptor.determine_blocksize}"
+        puts "Random prefix: #{decryptor.measure_prefix(decryptor.determine_blocksize)}"
+        puts "Using ECB = #{ECBDetector.detect(oracle.encrypt("a" * 200))}"
+        puts decryptor.decrypt
+
     end
 
 end
